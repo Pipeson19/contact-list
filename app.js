a4fd10fdf41831d16c4ed6f2a16d7c58e6d7631f
Felipe Salazar <felipesalazar19@hotmail.com>
2023-04-17 19:17:46 -0500

    actualizacion de todo

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-// Declaración de una lista vacía para almacenar los contactos
-let contactos = [];
-
-// Función para crear un nuevo contacto
-function crearContacto(id, nombres, apellidos, telefono, ubicaciones) {
-  // Crear un objeto de contacto con las propiedades proporcionadas
-  const contacto = {
-    id: '',
-    nombres: '',
-    apellidos: '',
-    telefono: '',
-    ubicaciones: ''
-  };
-  // Agregar el objeto de contacto a la lista de contactos
+class Contacto {
+  constructor(id, nombres, apellidos, telefono, ubicaciones, ciudad, direccion) {
+    this.id = id;
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+    this.telefono = telefono;
+    this.ubicaciones = ubicaciones;
+    this.ciudad = ciudad;
+    this.direccion = direccion;
+  }
+}
+function agregarContacto(id, nombres, apellidos, telefono, ubicaciones, ciudad, direccion) {
+  const contacto = new Contacto(id, nombres, apellidos, telefono, ubicaciones, ciudad, direccion);
   contactos.push(contacto);
 }
-
-// Función para eliminar un contacto por su ID
 function eliminarContacto(id) {
-  // Buscar el índice del contacto con el ID proporcionado
   const indice = contactos.findIndex(contacto => contacto.id === id);
-  // Si se encuentra el contacto, eliminarlo de la lista de contactos
   if (indice !== -1) {
     contactos.splice(indice, 1);
   }
 }
-
-console.log(contactos)
